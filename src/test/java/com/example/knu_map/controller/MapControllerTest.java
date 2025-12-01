06ae30ce972d89b8c99305b5c84082d641e9f2e8
ibtSdan <ibtsdan@gmail.com>
2025-12-01 21:51:28 +0900

    refactor: db 호스트 변경

--- src/test/java/com/example/knu_map/controller/MapControllerTest.java
+++ src/test/java/com/example/knu_map/controller/MapControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @Transactional
 @Import(TestConfig.class)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class MapControllerTest {
 
