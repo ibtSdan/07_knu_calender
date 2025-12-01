2735e3054e16454332ee0588df77eef3a7bacb04
ibtSdan <ibtsdan@gmail.com>
2025-12-01 19:05:26 +0900

    refactor: validation.properties 못읽음 수정

--- src/test/java/com/example/knu_map/controller/MapControllerTest.java
+++ src/test/java/com/example/knu_map/controller/MapControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @Transactional
+@TestPropertySource("classpath:validation.properties")
 @AutoConfigureMockMvc
 public class MapControllerTest {
 
