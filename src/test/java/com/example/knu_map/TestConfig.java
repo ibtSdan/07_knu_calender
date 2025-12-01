95ccc8ef40e2352b354d15f6ca6ada8daed3e541
ibtSdan <ibtsdan@gmail.com>
2025-12-01 19:43:38 +0900

    refactor: validation.properties testConfig 추가

---
+++ src/test/java/com/example/knu_map/TestConfig.java
@@ -0,0 +1,17 @@
+package com.example.knu_map;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@TestConfiguration
+public class TestConfig {
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:validation");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
--- src/test/java/com/example/knu_map/controller/MapControllerTest.java
+++ src/test/java/com/example/knu_map/controller/MapControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.knu_map.controller;
 
+import com.example.knu_map.TestConfig;
 import com.example.knu_map.domain.Category;
 import com.example.knu_map.exception.ErrorCode;
 import org.junit.jupiter.api.Nested;
@@ -9,6 +10,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @Transactional
-@TestPropertySource(locations = "classpath:validation.properties")
+@Import(TestConfig.class)
 @AutoConfigureMockMvc
 public class MapControllerTest {
 
